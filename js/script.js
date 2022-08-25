718b5723b48844b9a6e6bb98c8f47bceafb6dbb2
Johnnyy91 <claudiosardilli@gmail.com>
2022-08-25 15:36:54 +0200

    file creation

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+console.log('inizio');
+
+let i;
+
+for (i = 0; i < 100; i++) {
+
+    console.log(i);
+
+}
+
+console.log('fine', i);
+
+if (i.value / 3) 
+{
+    i = fizz
+    console.log(i)
+}
+
+else if (i.value / 5)
+{
+    i = buzz
+    console.log(i)
+}
+
